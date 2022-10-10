3c187b021933ee0b874eeef012267a67a55b57d3
Charlee Gordon <cgordon5025@gmail.com>
2022-10-10 15:45:18 -0400

    played around with functionality for save functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,48 @@
 // curl -X GET "https://www.dnd5eapi.co/api/ability-scores/cha" -H "Accept: application/json"
-var DndURL = "https://www.dnd5eapi.co/api/"
-async function DndAPI() {
-    let myData = fetch(DndURL)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data)
-            return data
-        })
-    return myData
+var saveAbilBtn = document.getElementById('saveAbilScores')
+var abilityScoreEl = $(".scoreInput");
+var modsEl = $(".mod")
+console.log(modsEl)
+console.log(abilityScoreEl)
+var savedScores = []
+var mods = []
+saveAbilBtn.addEventListener('click', SaveAbilScore)
+
+function SaveAbilScore() {
+    for (let i = 0; i < abilityScoreEl.length; i++) {
+        if (abilityScoreEl[i].value % 2 === 0) {
+            mods.push(abilityScoreEl[i] / 2)
+        } else {
+            mods.push((abilityScoreEl[i] / 2) - 1)
+        }
+        savedScores.push(abilityScoreEl[i].value)
+        modsEl[i].textContent = (mods[i])
+    }
+
+    localStorage.setItem("mySavedScore", JSON.stringify(savedScores))
 }
-function init() {
-    DndAPI().then(console.log(data))
+function abilityMod() {
+    for (let i = 0; i < abilityScoreEl.length; i++) {
+        if (abilityScoreEl[i].value % 2 === 0) {
+            mods.push(abilityScoreEl[i] % 2)
+        } else {
+            mods.push((abilityScoreEl[i] % 2) - 1)
+        }
+    }
 }
-init()
+// var DndURL = "https://www.dnd5eapi.co/api/"
+// async function DndAPI() {
+//     let myData = fetch(DndURL)
+//         .then(function (response) {
+//             return response.json()
+//         })
+//         .then(function (data) {
+//             console.log(data)
+//             return data
+//         })
+//     return myData
+// }
+// function init() {
+//     DndAPI().then(console.log(data))
+// }
+// init()
